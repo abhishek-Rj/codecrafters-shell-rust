68df26d41138f0e12c82d8c30ad3725ab710178d
abhishek-Rj <mahitomaki96@gmail.com>
2026-05-31 00:01:09 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+enum Command {
+    Echo,
+    Type,
+    Exit
+}
+
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
     loop {
@@ -23,17 +29,45 @@ fn main() {
     }
 }
 
-fn check_command(command: &str, res_args: &[String]) {
+fn parser(command: &str) -> Result<Command, String> {
     if command == "echo" {
-        for i in 0..res_args.len() {
-            print!("{}", res_args[i]);
-            if i != res_args.len() - 1 {
-                print!(" ");
+        Ok(Command::Echo)
+    } else if command == "type" {
+        Ok(Command::Type)
+    } else if command == "exit" {
+        Ok(Command::Exit)
+    } else {
+        Err("error parsing the command".into())
+    }
+}
+
+fn check_command(command: &str, res_args: &[String]) {
+    if let Ok(command)= parser(command) {
+        match command {
+            Command::Echo => {
+                for i in 0..res_args.len() {
+                    print!("{}", res_args[i]);
+                    if i != res_args.len() - 1 {
+                        print!(" ");
+                    }
+                }
+                println!("");
+                io::stdout().flush().unwrap();
+            },
+            Command::Exit => {
+                ()
+            }
+            Command::Type => {
+                if res_args.len() == 1 {
+                    if let Ok(_) = parser(res_args[0].as_str()) {
+                        println!("{} is a shell builtin", res_args[0]);
+                    } else {
+                        eprintln!("invalid_command: not found");
+                    }
+                }
             }
         }
-        println!("");
-        io::stdout().flush().unwrap();
     } else {
-        eprintln!("{}: command not found", command);
+        eprintln!("{}: command not found", command)
     }
 }
