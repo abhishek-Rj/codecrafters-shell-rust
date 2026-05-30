a6c6d42716ffc9173bb98ff124761e0bec91d5c0
abhishek-Rj <mahitomaki96@gmail.com>
2026-05-30 18:07:17 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,30 @@ fn main() {
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
 
+        if command.trim().is_empty() {
+            continue;
+        }
+
         if command.trim() == String::from("exit") {
             break;
         }
-    
-        eprintln!("{}: command not found", command.trim());
+
+        let args: Vec<String> = command.trim().split(" ").map(|s| s.to_string()).collect();
+        check_command(args[0].as_str(), &args[1..]);
+    }
+}
+
+fn check_command(command: &str, res_args: &[String]) {
+    if command == "echo" {
+        for i in 0..res_args.len() {
+            print!("{}", res_args[i]);
+            if i != res_args.len() - 1 {
+                print!(" ");
+            }
+        }
+        println!("");
+        io::stdout().flush().unwrap();
+    } else {
+        eprintln!("{}: command not found!", command);
     }
 }
