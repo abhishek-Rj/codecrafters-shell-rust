fe5c09cc50553922a8d242313aab08e04b55266e
abhishek-Rj <mahitomaki96@gmail.com>
2026-06-01 17:40:13 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 #[allow(unused_imports)]
+use std::fmt::format;
 use std::io::{self, Write};
+use std::process::Command;
 use std::{env, path::Path};
 use std::os::unix::fs::PermissionsExt;
 
-enum Commands {
+enum BuiltInCommands {
+    Exit,
     Echo,
     Type,
-    Exit
+}
+
+enum Commands {
+    Builtin(BuiltInCommands),
+    External(String) 
 }
 
 fn main() {
@@ -27,26 +34,44 @@ fn main() {
         }
 
         let args: Vec<String> = command.trim().split(" ").map(|s| s.to_string()).collect();
-        check_command(args[0].as_str(), &args[1..]); 
+        parser(args[0].as_str(), &args[1..]); 
     }
 }
 
-fn parser(command: &str) -> Result<Commands, String> {
+fn lexer(command: &str) -> Result<Commands, String> {
     if command == "echo" {
-        Ok(Commands::Echo)
+        Ok(Commands::Builtin(BuiltInCommands::Echo))
     } else if command == "type" {
-        Ok(Commands::Type)
+        Ok(Commands::Builtin(BuiltInCommands::Type))
     } else if command == "exit" {
-        Ok(Commands::Exit)
+        Ok(Commands::Builtin(BuiltInCommands::Exit))
     } else {
-        Err("error parsing the command".into())
+        match env::var("PATH") {
+            Ok(path) => {
+                let directories: Vec<String> = path.split(":").map(|s| s.to_string()).collect();
+                for dir in directories {
+                    let path = format!("{}/{}", dir, command);
+
+                    if if_file_exist_and_executable(&path) {
+                        return Ok(Commands::External(path));
+                    }
+                }
+                return Err("Coudn't parse the keyword".into());
+            },
+            Err(_) => {
+                return Err("Coudn't parse the keyword".into());
+            },
+        }
     }
 }
 
-fn check_command(command: &str, res_args: &[String]) {
-    if let Ok(command)= parser(command) {
-        match command {
-            Commands::Echo => {
+fn parser(command: &str, res_args: &[String]) {
+    if let Ok(cmmnd)= lexer(command) {
+        match cmmnd {
+            Commands::Builtin(BuiltInCommands::Exit) => {
+                ()
+            },
+            Commands::Builtin(BuiltInCommands::Echo) => {
                 for i in 0..res_args.len() {
                     print!("{}", res_args[i]);
                     if i != res_args.len() - 1 {
@@ -56,37 +81,43 @@ fn check_command(command: &str, res_args: &[String]) {
                 println!("");
                 io::stdout().flush().unwrap();
             },
-            Commands::Exit => {
-                ()
-            }
-            Commands::Type => {
+            Commands::Builtin(BuiltInCommands::Type) => {
                 if res_args.len() == 1 {
-                    if let Ok(_) = parser(res_args[0].as_str()) {
-                        println!("{} is a shell builtin", res_args[0]);
+                    if let Ok(cmd) = lexer(res_args[0].as_str()) {
+                        match cmd {
+                            Commands::Builtin(_) => {
+                                println!("{} is a shell builtin", res_args[0]);
+                                return;
+                            }
+                            Commands::External(path) => {
+                                println!("{} is {}", res_args[0], path);
+                                return;
+                            }
+                        }
+                    } else {
+                        eprintln!("{}: not found", res_args[0]);
                         return;
                     } 
-                    match env::var("PATH") {
-                        Ok(path) => {
-                            let directories: Vec<String> = path.split(":").map(|s| s.to_string()).collect();
-                            for dir in directories {
-                                let path = format!("{}/{}", dir, res_args[0]);
-                                
-                                if Path::new(&path).is_file() {
-                                    let metadata = std::fs::metadata(&path).unwrap();
-                                    if metadata.permissions().mode() & 0o111 != 0 {
-                                        println!("{} is {}", res_args[0], path);
-                                        return;
-                                    }
-                                }
-                            }
-                            eprintln!("{}: not found", res_args[0]);
-                        },
-                        Err(e) => eprintln!("Coudn't read PATH: {}", e),
-                    }
                 }
+            },
+            Commands::External(_) => {
+                let output = Command::new(command).args(&res_args[..]).output().expect("failed to execute program");
+                let stdout = output.stdout;
+                print!("{}", String::from_utf8_lossy(&stdout));
+                io::stdout().flush().unwrap();
             }
         }
     } else {
         eprintln!("{}: command not found", command)
     }
+}
+
+fn if_file_exist_and_executable(path: &String) -> bool {
+    if Path::new(path).is_file() {
+        let metadata = std::fs::metadata(path).unwrap();
+        if metadata.permissions().mode() & 0o111 != 0 {
+            return true
+        }
+    }
+    false
 }
